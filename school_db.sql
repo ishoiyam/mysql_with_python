47a05e35389f31226f2f28eb104335f1a3fcf8a5
ishoiyam <ishoiyam@protonmail.com>
2021-11-03 16:07:18 +0100

    database inserted

---
+++ school_db.sql
@@ -0,0 +1,22 @@
+drop database if exists school_db;
+create database school_db;
+use school_db;
+
+--- school_system (no graphical interface for now)
+-- login (username, password)
+-- show some output (loged in successfully)
+--      show all students
+--      select a single student and show his information
+--      update / delete / edit a student
+-- === do this for teachers and class
+
+--- table student
+--- table teacher
+--- table class
+
+
+
+-- relations between student and class
+-- class & teacher
+-- teacher & student
+-- join between tables - order by - sort by
